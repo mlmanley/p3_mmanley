ea479d9b57179934127ce11793543dd6caa351a4
Maira Manley <mlememanley@ufl.edu>
2022-04-18 19:18:46 -0400

    fixed syling issues for desktop

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,62 @@
-$(document).ready(function() {
-    $('.hamburger').on('click', function() {
-        $('.top-nav').slideToggle('fast','linear');
-    });
+$(document).ready(function () {
+  $(".hamburger").on("click", function () {
+    $(".top-nav").slideToggle("fast", "linear");
+  });
 
-    var url = window.location.href;
+  var url = window.location.href;
 
-    if (url.indexOf("menu") > -1) {
+  if (url.indexOf("menu") > -1) {
+    // Add smooth scrolling to all links
+    $("a").on("click", function (event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
 
-        // Add smooth scrolling to all links
-        $("a").on('click', function(event) {
-    
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-    
-            // Store hash
-            var hash = this.hash;
-            console.log(hash);
-    
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-            scrollTop: $(hash).offset().top
-            }, 800, function(){
-    
+        // Store hash
+        var hash = this.hash;
+        console.log(hash);
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $("html, body").animate(
+          {
+            scrollTop: $(hash).offset().top,
+          },
+          800,
+          function () {
             // Add hash (#) to URL when done scrolling (default click behavior)
             window.location.hash = hash;
-            });
-        } // End if
-        });
-    }
+          }
+        );
+      } // End if
+    });
+  }
+
+  $(".carousel").slick({
+    centerMode: true,
+    centerPadding: "40px",
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 2,
+        },
+      },
+
+      {
+        breakpoint: 821,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          // centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
+});
